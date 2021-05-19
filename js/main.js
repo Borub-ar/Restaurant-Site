95f3652953ebef9ed5521a79e713c1d57e443896
Kacper <barabasz64@gmail.com>
2021-05-19 21:40:06 +0200

    Add navbar styling and funkcionality

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', () => {
+    const navBar = document.querySelector('.menu');
+    const navBarLogo = document.querySelector('.menu__logo');
+
+    if (window.pageYOffset > navBar.getBoundingClientRect().height) {
+        navBarLogo.style.maxHeight = '2.5rem';
+    }
+
+    if (window.pageYOffset < navBar.getBoundingClientRect().height) {
+        navBarLogo.style.maxHeight = '4.6rem';
+    }
+})
